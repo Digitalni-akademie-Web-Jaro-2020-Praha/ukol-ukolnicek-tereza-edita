d66fef978bb6e891fc7f562494b0e6efb1e33fa0
Tereza E <t.e.hronova@gmail.com>
2020-05-04 22:03:09 +0200

    Hotovo.

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-'use strict';
+'use strict'
 
-console.log('funguju!');
+console.log('funguju!')
+
+let tasks = []
+const toDo = document.querySelector('.todo__tasks')
+
+const updateTasks = () => {
+  toDo.innerHTML += `
+  <div class="task">
+    ${tasks[tasks.length - 1]}
+    </div>`
+}
+
+const btnAdd = document.querySelector('.btn-add')
+btnAdd.addEventListener('click', () => {
+  const newTask = document.querySelector('.new-task').value
+  tasks.push(newTask)
+  updateTasks(tasks)
+})
